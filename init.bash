1727d39d97387bd0fe20c9e4c79b037dd90739b8
kipp <kipphulick@gmail.com>
2022-08-30 11:28:07 -0500

    models and migrations done

--- init.bash
+++ init.bash
@@ -1,1 +1,7 @@
 # npm i express ejs passport@0.5 passport-local bcryptjs cookie-session helmet pg pg-hstore sequelize
+
+# sequelize model:generate --name roles --attributes name:string
+
+# sequelize model:generate --name users --attributes username:string,email:string,password:string,roleID:integer
+
+# sequelize model:generate --name records --attributes userID:integer,
