391dbc8538fe8e25c986a134797a356f72c10bec
kipp <kipphulick@gmail.com>
2022-08-31 18:49:24 -0500

    working authentication with google Oauth

--- auth/passport-config.js
+++ auth/passport-config.js
@@ -1,24 +1,25 @@
-const localStrategy = require('passport-local')//.Strategy;
+const localStrategy = require('passport-local')
 const GoogleStrategy = require('passport-google-oidc')
 const bcrypt = require('bcryptjs');
 const db = require('../models')
 
+//! replace these variables with .env variables
 GOOGLE_CLIENT_ID = '907968964497-e8jrp7mfu09fadm1ep4iibefm0t6tkgm.apps.googleusercontent.com'
 GOOGLE_CLIENT_SECRET = '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'
 
 const init = (passport) => {
 
+    // Local authentication strategy
     passport.use(new localStrategy({usernameField: 'email'}, async (email, password, done) => {
         try {
-            // console.log(password);
+            // check if there's a user with that email
             let records = await db.users.findAll({where: {email:email}})
-            // console.log(records);
 
             if (records){
+                //found a matching email
                 let record = records[0]
-                // console.log(record);
-                
 
+                //checking if passwords match
                 bcrypt.compare(password, record.password, (err, match) => {
                     if(match){
                         console.log('passwords match!');
@@ -43,88 +44,26 @@ const init = (passport) => {
         }
     }))
 
-    //! in production mode we need to import these variables from a .env file
-    // passport.use(new googleStrategy({
-    //     clientID: '907968964497-e8jrp7mfu09fadm1ep4iibefm0t6tkgm.apps.googleusercontent.com',
-    //     clientSecret: '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',
-    //     callbackURL: "http://localhost:3000/login",
-    //     passReqToCallback: true
-    // }, authUser))
-
-    // passport.use(new GoogleStrategy({
-    //     clientID: process.env[GOOGLE_CLIENT_ID],
-    //     clientSecret: process.env[GOOGLE_CLIENT_SECRET],
-    //     callbackURL: 'https://localhost:3000/google/callback',
-    //     passReqToCallback: true
-    //   },
-    //   function(issuer, profile, cb) {
-    //     db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [ //! check if in db
-    //       issuer,
-    //       profile.id
-    //     ], function(err, cred) {
-    //       if (err) { return cb(err); }
-    //       if (!cred) {
-    //         // The Google account has not logged in to this app before.  Create a
-    //         // new user record and link it to the Google account.
-    //         db.run('INSERT INTO users (name) VALUES (?)', [ //! create a new user !\\
-    //           profile.displayName
-    //         ], function(err) {
-    //           if (err) { return cb(err); }
-    
-    //           var id = this.lastID;
-    //           db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
-    //             id,
-    //             issuer,
-    //             profile.id
-    //           ], function(err) {
-    //             if (err) { return cb(err); }
-    //             var user = {
-    //               id: id.toString(),
-    //               name: profile.displayName
-    //             };
-    //             return cb(null, user);
-    //           });
-    //         }); //!\\
-    //       } else {
-    //         // The Google account has previously logged in to the app.  Get the
-    //         // user record linked to the Google account and log the user in.
-    //         db.get('SELECT * FROM users WHERE id = ?', [ cred.user_id ], function(err, user) {
-    //           if (err) { return cb(err); }
-    //           if (!user) { return cb(null, false); }
-    //           return cb(null, user);
-    //         });
-    //       }
-    //     });
-    //   }
-    // ));
-
+    // Google authentication strategy
     passport.use(new GoogleStrategy({
         clientID: GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: 'https://localhost:3000/google/callback',
-      },
-      async function(refreshToken, profile, done){
-
-        console.log(done);
-        console.log({refreshToken, profile, done});
-        let email = profile.emails[0].value
-        // let testProf = await db.users.findByPk(1)
-        // console.log(accessToken);
-
+        callbackURL: 'http://localhost:3000/google/callback',
+    },
+    async function(refreshToken, profile, done){
         try {
-            // console.log(password);
+            let email = profile.emails[0].value
             let records = await db.users.findAll({where: {email}})
 
             if (records[0]){
-                console.log(records[0]);
-                console.log('logging into existing account');
-                //user already exists in our database
-                return done(null, records[0])
+                //user already exists in our database we return the user
+                console.log('logging into existing google account');
 
+                return done(null, records[0])
             }
             else{
+                //user has never logged in so we create a new account
                 console.log('creating new user from google email');
-                //new user so we create a new user
                 let insertRecord = await db.users.create({email})
                 let user = await db.users.findAll({where: {email}})
 
@@ -136,10 +75,7 @@ const init = (passport) => {
             return done(error)
         }
 
-
-        // return done(null, testProf)
-      }))
-
+    }))
 
     passport.serializeUser((user, done) => {
         console.log(user.dataValues);
