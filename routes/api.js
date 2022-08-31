fa081d2194d7d04f9904b8e819ce9c36d538f29a
kipp <kipphulick@gmail.com>
2022-08-31 17:03:34 -0500

    have to be authenticated to call the api

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const auth = require('../auth');
 const db = require('../models');
 
-router.get('/api', async (req,res) => {
-    
+router.get('/api', auth.authReq, async (req,res) => {
+
     console.log('accessing the api');
     let records = await db.records.findAll({where :{ 'userID': req.user.id }})
    
