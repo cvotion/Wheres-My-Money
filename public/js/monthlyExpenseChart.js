df690ba52a94e92efe7fce076eef2db0b26da4b5
Christian Votion <christianvotion@gmail.com>
2022-09-01 11:27:35 -0500

    added doughtnut chart and two bar charts to index page

---
+++ public/js/monthlyExpenseChart.js
@@ -0,0 +1,35 @@
+const expenseData = {
+    labels: [
+      'Last Month',
+      'This Month'
+    ],
+    datasets: [{
+      label: 'Expense Comparison',
+      data: [19050,21020],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)'
+        
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  const expenseConfig = {
+    type: 'bar',
+    data: expenseData,
+    options: {
+        indexAxis: 'y',
+        animation: {
+            animateScale: true
+        },
+        responsive: true,
+        maintainAspectRatio: false
+    }
+  };
+
+  const expenseChart = new Chart(
+    document.getElementById('monthly-Expense-Chart'),
+    expenseConfig
+);
+    
