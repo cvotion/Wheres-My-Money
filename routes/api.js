5ab557f971fe616a7c4394729c7c53895c22c9dc
kipp <kipphulick@gmail.com>
2022-09-01 11:08:50 -0500

    added try catch to get api

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const auth = require('../auth');
@@ -6,10 +5,16 @@ const db = require('../models');
 
 router.get('/api', auth.authReq, async (req,res) => {
 
-    console.log('accessing the api');
+    try {
+        console.log('accessing the api');
     let records = await db.records.findAll({where :{ 'userID': req.user.id }})
    
-    res.json({records})
+    res.status(200).json({records})
+
+    } catch (error) {
+        res.status(400).json({error})
+    }
+    
 })
 
 router.post('/api', auth.authReq, async (req,res) => {
