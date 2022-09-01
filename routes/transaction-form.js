09cffe3966a4722713f75792a006ee7289e87123
kipp <kipphulick@gmail.com>
2022-09-01 15:41:16 -0500

    fixed form page

--- routes/transaction-form.js
+++ routes/transaction-form.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../auth');
 
-router.get('/form', (req, res) => {
-    res.render('transaction-form')
+router.get('/form', auth.authReq, (req, res) => {
+    res.render('transaction-form', {user: req.user})
 })
 
 module.exports = router;
