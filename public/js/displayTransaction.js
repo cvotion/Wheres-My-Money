4785a620a6dab9f8212621dfb393c16be9e6a6ae
Joy He <joyhe520@gmail.com>
2022-09-01 11:29:00 -0500

    display transaction table works

--- public/js/displayTransaction.js
+++ public/js/displayTransaction.js
@@ -5,10 +5,27 @@ let displayTransaction = document.querySelector(".transaction-appendhere");
 const getTransactionRecord = async () =>{
     let result = await fetch('http://localhost:3000/api');
     let resultRecord = await result.json();
-    console.log(resultRecord);
-    // resultRecord.forEach(eachTransaction=>{
-    //     console.log(eachTransaction);
-    // })
+    let recordArr = resultRecord.records
+    console.log(recordArr);
+    let eachDisplay = ""
+    recordArr.forEach(eachTransaction=>{
+        console.log(eachTransaction);
+        console.log(eachTransaction.description);
+        
+        eachDisplay +=`
+            <tr>
+                
+                <td class="text-center">${eachTransaction.date}</td>
+                <td>${eachTransaction.description}</td>
+                <td>${eachTransaction.amount}</td>
+                <td>${eachTransaction.type}</td>
+                <td>${eachTransaction.category}</td>
+            
+            </tr>
+        `
+        displayTransaction.innerHTML = eachDisplay
+    })
 }
 
 getTransactionRecord()
+
